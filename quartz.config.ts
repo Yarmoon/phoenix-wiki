eafd8210eba90811f0769211a6cd8616d0095002
Yarmoon <jar.lev@yandex.ru>
2025-05-19 18:45:43 +0300

    Quartz sync: May 19, 2025, 6:45 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Quartz 4",
+    pageTitle: "PhoenixWiki",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
@@ -56,6 +56,7 @@ const config: QuartzConfig = {
   plugins: {
     transformers: [
       Plugin.FrontMatter(),
+	  Plugin.HardLineBreaks(),
       Plugin.CreatedModifiedDate({
         priority: ["frontmatter", "git", "filesystem"],
       }),
