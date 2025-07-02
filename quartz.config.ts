f8168bc08a02ee0c4a0c7fa449deafbbab37ae76
Yarmoon <jar.lev@yandex.ru>
2025-07-02 21:55:08 +0300

    Quartz sync: Jul 2, 2025, 9:55 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -29,17 +29,6 @@ const config: QuartzConfig = {
       },
       colors: {
         lightMode: {
-          light: "#faf8f8",
-          lightgray: "#e5e5e5",
-          gray: "#b8b8b8",
-          darkgray: "#4e4e4e",
-          dark: "#2b2b2b",
-          secondary: "#284b63",
-          tertiary: "#84a59d",
-          highlight: "rgba(143, 159, 169, 0.15)",
-          textHighlight: "#fff23688",
-        },
-        darkMode: {
           light: "#161618",
           lightgray: "#393639",
           gray: "#625b5b",
